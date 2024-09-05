90b9409078e3213b0f05d9af111de0ebeb32503c
lovisahansson <lovisa.hansson2@gmail.com>
2024-09-05 09:54:23 +0200

    Start willow

---
+++ Willow.R
@@ -0,0 +1,26 @@
+#------------------------------------------------------------------------------
+# BI1339 - Willow
+# Lovisa Hansson
+# 2024-09-05
+#------------------------------------------------------------------------------
+
+setwd("~/R/BI1339/BI1339")
+
+install.packages('tidyverse')
+install.packages('ggpubr')
+install.packages('patchwork')
+install.packages('cowplot')
+install.packages('viridis')
+install.packages('usethis')
+#install.packages('PerformaceAnalytics')
+#install.packages('lme4')
+
+library('tidyverse')
+library('ggpubr')
+library('patchwork')
+library('cowplot')
+library('viridis')
+#library('PerformanceAnalytics')
+#library('lme4')
+
+#------------------------------------------------------------------------------
